d4d2232a2a118a7200e8ffdfa23a9300f825bdcd
Ruslan <126184381+IvanovvvRuslan@users.noreply.github.com>
2025-09-07 16:37:31 +0300

    First commit

---
+++ ContactManagerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ContactManagerApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagerApp.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) {}
+
+    public DbSet<Contact> Contacts { get; set; }
+}
---
+++ ContactManagerApp/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactManagerApp.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public DateTime BirthDate { get; set; }
+    
+    public bool IsMarried { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public decimal Salary { get; set; }
+}
